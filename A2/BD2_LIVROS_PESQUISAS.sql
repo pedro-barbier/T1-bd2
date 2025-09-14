e7fa3a2d86258d76bb47caf1ee785ec364b89533
barbier1 <pedro.barbieri@edu.pucrs.br>
2025-09-14 17:29:23 -0300

    Enunciado
    Adição das questões e seus enunciados

---
+++ A2/BD2_LIVROS_PESQUISAS.sql
@@ -0,0 +1,14 @@
+---- 1 )
+-- a) Apresentar a quantidade de clientes em cada região.
+
+-- b) Listar o nome dos 10 produtos mais vendidos por quantidade em ordem decrescente.
+
+-- c) Apresentar os 5 pedidos com valor mais alto e indicar o nome do funcionário que atendeu o pedido.
+
+-- d) Listar as categorias que não possuem categoria pai e a quantidade de produtos associados a cada uma.
+
+-- e) Listar todos os produtos e seus fornecedores, apresentando apenas os que constam como ativos.
+
+-- f) Apresentar as entregas com os dados de endereço, nome do cliente e ordenados por data de entrega.
+
+---- 2 )
